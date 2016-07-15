9d48286d3fac512df5db88eb5ecbbb9c31f06cab
Alvi <fviguest@fvi.edu>
2016-07-15 13:08:05 -0400

    score

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ var mainState = {
                 Phaser.Keyboard.SPACEBAR);
       spaceKey.onDown.add(this.jump, this);
       this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+      this.score = 0;
+this.labelScore = game.add.text(20, 20, "0",
+    { font: "30px Arial", fill: "#ffffff" });
     },
 
     update: function() {
